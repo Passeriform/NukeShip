b2af18aa78d681d2194b9e90b2a8e9629eb40b11
Passeriform <bhardwajutkarsh.ub@gmail.com>
2025-05-24 14:55:25 +0530

    [Utility] Remove unused tween helpers, add bounds helpers

--- pkg/client/frontend/src/utility/bounds.ts
+++ pkg/client/frontend/src/utility/bounds.ts
@@ -1,8 +1,6 @@
-import { Box3, Object3D, Vector3 } from "three"
+import { Box3, Mesh, Object3D, Vector3, Vector3Tuple } from "three"
 
 const _bounds = new Box3()
-const _boundCenter = new Vector3()
-const _boundSize = new Vector3()
 
 export const boundsFromObjects = (...objects: Object3D[]) => {
     _bounds.makeEmpty()
@@ -10,8 +8,26 @@ export const boundsFromObjects = (...objects: Object3D[]) => {
     return _bounds.clone()
 }
 
-export const unpackBounds = (bounds: Box3) => {
-    bounds.getCenter(_boundCenter)
-    bounds.getSize(_boundSize)
-    return [_boundCenter.clone(), _boundSize.clone()] as [Vector3, Vector3]
+export const boundsFromPoints = (...points: Vector3Tuple[]) => {
+    _bounds.makeEmpty()
+    points.forEach((point) => _bounds.expandByPoint(new Vector3(...point)))
+    return _bounds.clone()
+}
+
+export const boundsFromMeshGeometries = (...meshes: Mesh[]) => {
+    _bounds.makeEmpty()
+    meshes.forEach((mesh) => {
+        const meshBoundingBox = new Box3()
+
+        mesh.updateWorldMatrix(false, false)
+
+        if (!mesh.geometry.boundingBox) {
+            mesh.geometry.computeBoundingBox()
+        }
+
+        meshBoundingBox.copy(mesh.geometry.boundingBox!)
+        meshBoundingBox.applyMatrix4(mesh.matrixWorld)
+        _bounds.union(meshBoundingBox)
+    })
+    return _bounds.clone()
 }
