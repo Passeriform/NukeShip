ec44cecced35911dd4d468ca90f6a52540ad6e7a
Passeriform <bhardwajutkarsh.ub@gmail.com>
2025-07-30 03:09:26 +0530

    [UI] Force fix nil-able bound enum state

--- pkg/client/frontend/src/hooks/useGameState.tsx
+++ pkg/client/frontend/src/hooks/useGameState.tsx
@@ -1,10 +1,10 @@
 import { createResource, onCleanup, onMount } from "solid-js"
 import { GetRoomState } from "@wails/go/main/WailsApp"
-import { main } from "@wails/go/models"
+import { main, pb } from "@wails/go/models"
 import { EventsOff, EventsOn } from "@wails/runtime/runtime"
 
 const useGameState = () => {
-    const [gameState, { refetch }] = createResource(GetRoomState)
+    const [gameState, { refetch }] = createResource<pb.RoomState | undefined>(GetRoomState)
 
     onMount(() => {
         EventsOn(main.Event.STATE_CHANGE, refetch)
